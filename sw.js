122ceaf471c25aee6f3f962398fabfa00ed187fa
vincerob213-lgtm <vince.rob213@gmail.com>
2026-05-12 19:04:53 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'calc-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const FILES = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
